6c9bb00711fc678d3212903ecb4623a8e8a62fa9
= <minjae10005@kakao.com>
2024-11-27 14:58:14 +0900

    Update logging

--- setup.sh
+++ setup.sh
@@ -15,8 +15,8 @@ echo "Ubuntu system detected. Proceeding with setup..."
 
 # 필요한 패키지 설치 (출력 최소화)
 echo "Installing required packages..."
-sudo apt-get update -qq
-sudo apt-get install -y -qq golang-go clang llvm linux-headers-$(uname -r) libbpf-dev gcc-multilib
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install -y golang-go clang llvm linux-headers-$(uname -r) libbpf-dev gcc-multilib > /dev/null 2>&1
 
 # ./server 폴더로 이동하여 go generate 실행
 echo "Running 'go generate' in ./server directory..."
